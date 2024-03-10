7e7b0de8fbb3df7b9e633e9c37c186b98e7c2503
RicaflancaMarkJimuel <markjimuelricaflanca@gmail.com>
2024-03-10 17:14:54 +0800

    Home page is Added

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="Ricaflanca & Atienza" />
+        <title>Melomaniac Activity</title>
+
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Jim's Music </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song_1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song_2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song_3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song_4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song_5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melomaniac</h1><br>
+                <p class="lead">Music is the voice of the soul.</p>
+                <p class="lead">Where you can find good Music.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">Song Title here</h2>
+                        <p class="lead">display the Composer here</p>
+                        <p>display the lyrics/chords here</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-primary">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2024. Ricaflanca & Atienza</p></div>
+        </footer>
+
+    </body>
+</html>
